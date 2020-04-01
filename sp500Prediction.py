e22ea79f8d3a3f337d2a297909d75ce210e9c10e
Steven Wright <steven@Stevens-MacBook-Pro.local>
2020-03-31 19:56:15 -0700

    Add forecasting

--- sp500Prediction.py
+++ sp500Prediction.py
@@ -1,25 +1,78 @@
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
+import numpy as np
+import datetime
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import math
 import matplotlib.pyplot as plt
-from sklearn import linear_model
+from matplotlib import style
+style.use('ggplot')
 
 # Get data
 ticker = "SPY"
-my_key = "your API key"
+my_key = "your-API-key"
 ts = TimeSeries(key=my_key, output_format='pandas')
 data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize='full')
 
-#Graph
-data['4. close'].plot()
-plt.title('Intraday Times Series for '+ ticker +'(1 min)')
-plt.show()
-
-# Create dataframe, drop null values, set X and y values
+# Create dataframe, drop null values
 df = pd.DataFrame(data)
-df.dropna(how='any', inplace=True)
-X = df.drop(columns = ['4. close'], axis = 1)
-y = df['4. close']
+df.fillna(value=-99999, inplace=True)
+forecast_col = '4. close'
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+print(df.head())
+
+#Set X and y values
+X = np.array(df.drop(columns = ['label'], axis = 1))
+X = preprocessing.scale(X)
+
+#What we will forecast against
+X_lately = X[-forecast_out:]
+
+#The rest of the X data, used for the regression model and confidence
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 #Regression
-clf = linear_model.LinearRegression()
-clf.fit(X, y)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+
+#Confidence
+confidence = clf.score(X_test, y_test)
+
+#Forecasting
+forecast_set = clf.predict(X_lately)
+
+#Results
+print(forecast_set, confidence, forecast_out)
+
+#Add forecast column
+df['Forecast'] = np.nan
+
+#Get last time in dataframe so we can assign a new time to each forecast
+last_time = df.iloc[-1].name
+last_unix = last_time.timestamp()
+one_minute = 60
+next_unix = last_unix + one_minute
+
+#Set next row values in forecast dataframe
+for i in forecast_set:
+    next_time = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 60
+    df.loc[next_time] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+#Graph
+df['4. close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
